cdbb2780748c7b6f1efd62a9c039595bed26ad2b
renanrgarcia <renanrgarcia@gmail.com>
2024-07-18 13:32:41 -0300

    1.java-class-anatomy - Variables and methods

--- 1.java-basic-syntax/1.java-class-anatomy/src/MyClass.java
+++ 1.java-basic-syntax/1.java-class-anatomy/src/MyClass.java
@@ -2,6 +2,16 @@ public class MyClass {
   public static void main(String[] args) {
     // System.out.println("Hello, my World!");
 
-    String BR = "Brasil";
+    // String BR = "Brasil";
+
+    String name = "Renan";
+    String lastName = "Garcia";
+    String completeName = completeName(name, lastName);
+    System.out.println(completeName);
+
+  }
+
+  public static String completeName (String name, String lastName) {
+    return "Hello, my name is " + name.concat(" ").concat(lastName);
   }
 }
