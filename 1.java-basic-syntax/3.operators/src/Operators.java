7453468a3ec0c35fe0dc17c46bec036b73dbccaf
renanrgarcia <renanrgarcia@gmail.com>
2024-07-19 13:54:38 -0300

    3.Operators - Unary operators

--- 1.java-basic-syntax/3.operators/src/Operators.java
+++ 1.java-basic-syntax/3.operators/src/Operators.java
@@ -1,16 +1,7 @@
 public class Operators {
     public static void main(String[] args) throws Exception {
-        String concatenacao ="?"; 
-        System.out.println(concatenacao);
-        concatenacao = 1+1+1+"1";
-        System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+1;
-        System.out.println(concatenacao);
-        concatenacao = 1+"1"+1+"1";
-        System.out.println(concatenacao);
-        concatenacao = "1"+1+1+1;
-        System.out.println(concatenacao);
-        concatenacao = "1"+(1+1+1);
-        System.out.println(concatenacao);
+        int number = 5;
+
+        System.out.println(-number);
     }
 }
