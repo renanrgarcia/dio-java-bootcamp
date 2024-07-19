7b31ba96bb56523f67478895b13f13f1bad463fb
renanrgarcia <renanrgarcia@gmail.com>
2024-07-19 13:49:26 -0300

    3.Operators - Atribution and arithmetic operators

---
+++ 1.java-basic-syntax/3.operators/src/Operators.java
@@ -0,0 +1,16 @@
+public class Operators {
+    public static void main(String[] args) throws Exception {
+        String concatenacao ="?"; 
+        System.out.println(concatenacao);
+        concatenacao = 1+1+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+        System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+"1";
+        System.out.println(concatenacao);
+        concatenacao = "1"+1+1+1;
+        System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+        System.out.println(concatenacao);
+    }
+}
