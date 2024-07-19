122d82a102e9f2cf4e7718d25d0046479e38d79f
renanrgarcia <renanrgarcia@gmail.com>
2024-07-19 14:13:42 -0300

    3.Operators - Changing variable values

--- 1.java-basic-syntax/3.operators/src/Operators.java
+++ 1.java-basic-syntax/3.operators/src/Operators.java
@@ -2,6 +2,18 @@ public class Operators {
     public static void main(String[] args) throws Exception {
         int number = 5;
 
-        System.out.println(-number);
+        // number = number++;
+        System.out.println(number++); // print 5 and increment later
+
+        System.out.println(number);
+
+        boolean variable = true;
+
+        System.out.println(!variable); // just in memory
+        System.out.println(variable); // value not changed
+
+        variable = !variable; // value changed
+
+        System.out.println(variable);
     }
 }
