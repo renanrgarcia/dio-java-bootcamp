6617fa823ace4213c873172cc92b62ebd0e92cba
renanrgarcia <renanrgarcia@gmail.com>
2024-07-21 18:47:50 -0300

    2.java-flux-control - Ternary operator

--- 2.java-flux-control/1.flux-control/src/ReportCard.java
+++ 2.java-flux-control/1.flux-control/src/ReportCard.java
@@ -1,13 +1,16 @@
 public class ReportCard {
   public static void main(String[] args) {
     int grade = 90;
-    if (grade >= 60) {
-      System.out.println("Passed");
-    } else if (grade >= 40 && grade < 60) {
-      System.out.println("Remedial");
-    } else {
-      System.out.println("Failed");
-    }
+    // if (grade >= 60) {
+    // System.out.println("Passed");
+    // } else if (grade >= 40 && grade < 60) {
+    // System.out.println("Remedial");
+    // } else {
+    // System.out.println("Failed");
+    // }
+
+    String result = grade >= 60 ? "Passed" : grade < 60 && grade >= 40 ? "Remedial" : "Failed";
+    System.out.println(result);
 
   }
 }
