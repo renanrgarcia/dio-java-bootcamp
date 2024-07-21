51ccdf68ab12da4a65f52556a383d5e6ac2e3449
renanrgarcia <renanrgarcia@gmail.com>
2024-07-21 06:48:50 -0300

    7.Java Docs - done

---
+++ 1.java-basic-syntax/7.java-docs/docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"Calculator","l":"add(int, int)","u":"add(int,int)"},{"p":"<Unnamed>","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Calculator","l":"Calculator()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"App","l":"main(String[])","u":"main(java.lang.String[])"}];updateSearchResults();
---
+++ 1.java-basic-syntax/7.java-docs/src/Calculator.java
@@ -0,0 +1,23 @@
+/**
+ * <h1>Calculator</h1>
+ * The Calculator performs mathematical operations between integers
+ * <p>
+ * <b>Note:</b> Read the documentation of this class carefully
+ * to enjoy the features offered by the author
+ *
+ * @author Gleyson Sampaio
+ * @version 1.0
+ * @since 01/01/2022
+ */
+public class Calculator {
+  /**
+   * This method is used to add two integers
+   * 
+   * @param firstNumber  this is the first parameter of the method
+   * @param secondNumber this is the second parameter of the method
+   * @return int the result of this method is the sum of the two numbers.
+   */
+  public int add(int firstNumber, int secondNumber) {
+    return firstNumber + secondNumber;
+  }
+}
